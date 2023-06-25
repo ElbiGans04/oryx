a7d5d409d15bdc5e1caa61d92780f346bfa9d782
ElbiGans04 <rhafaelbijaksana04@gmail.com>
2023-06-25 21:38:25 +0700

    fix countdown
    buat agar jika unit (hari, jam, menit, detik) dibawah 10 maka kita harus
    menambahkan 0 di bagian depannya

--- src/app/countdown.js
+++ src/app/countdown.js
@@ -8,11 +8,17 @@ export default function Countdown() {
     const selisih = new Date(2023, 6, 27).getTime() - new Date().getTime();
     if (selisih > 0) {
       const interval = setInterval(() => {
+        let hari = Math.floor(selisih / (1000 * 60 * 60 * 24));
+        let jam = Math.floor(
+          (selisih % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+        );
+        let menit = Math.floor((selisih % (1000 * 60 * 60)) / (1000 * 60));
+        let detik = Math.floor((selisih % (1000 * 60)) / 1000);
         setCountdown([
-          Math.floor(selisih / (1000 * 60 * 60 * 24)),
-          Math.floor((selisih % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)),
-          Math.floor((selisih % (1000 * 60 * 60)) / (1000 * 60)),
-          Math.floor((selisih % (1000 * 60)) / 1000),
+          hari < 10 ? `0${hari}` : hari,
+          jam < 10 ? `0${jam}` : jam,
+          menit < 10 ? `0${menit}` : menit,
+          detik < 10 ? `0${detik}` : detik,
         ]);
       }, 1000);
 
@@ -50,7 +56,9 @@ export default function Countdown() {
           key={value.key}
           className="flex flex-col justify-center items-center space-y-[16px] xl:space-y-[20px] font-chivo"
         >
-          <p className={`text-[32px] xl:text-[45px] text-white ${value.className}`}>
+          <p
+            className={`text-[32px] xl:text-[45px] text-white ${value.className}`}
+          >
             {countDown[index]}
           </p>
           <p className="text-[16px] xl:text-[24px] text-white">{value.key}</p>
